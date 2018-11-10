312df83d90db9caaffc57956b295d96fe67c0f4c
Abhinav <abhinav@Abhinavs-MacBook-Pro.local>
2018-11-10 16:08:25 -0500

    user email

--- app.js
+++ app.js
@@ -20,6 +20,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, authorization, X-HTTP-Method-Override');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,PUT,OPTIONS');
+    next();
+});
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
--- models/sessionModel.js
+++ models/sessionModel.js
@@ -6,10 +6,9 @@ const mongoose=require('mongoose');
 
 const sessionSchema=new mongoose.Schema({
     sessionId:{type:String,required:true,unique:true},
-    userEmail:{type:String},
+    userEmail:{type:String,default:null},
     createdAt:{type:Date,required:true,default:new Date()},
     isFulfilled:{type:Boolean,default:false}
 });
-
 
 module.exports = mongoose.model("sessions",sessionSchema);
